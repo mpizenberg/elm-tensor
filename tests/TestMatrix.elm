488b7265f68c0c73794554fb48b96fb793da81f2
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2018-01-09 17:59:43 +0800

    Test equal

--- tests/TestMatrix.elm
+++ tests/TestMatrix.elm
@@ -4,11 +4,32 @@ module TestMatrix
         )
 
 import Expect
+import Internal.Tensor as T
 import Matrix
 import Test exposing (..)
-import TestFuzz as Fuzz
+import TestFuzz
 
 
 equal : Test
 equal =
-    todo "equal"
+    describe "equal"
+        [ fuzz TestFuzz.matrix "is reflexive" <|
+            \matrix ->
+                T.equal matrix matrix
+                    |> Expect.true "should be: matrix == matrix"
+        , fuzz2 TestFuzz.matrix TestFuzz.matrix "is symmetric" <|
+            \m1 m2 ->
+                T.equal m1 m2
+                    |> Expect.equal (T.equal m2 m1)
+        , fuzz3 TestFuzz.matrix TestFuzz.matrix TestFuzz.matrix "is transitive" <|
+            \m1 m2 m3 ->
+                let
+                    ( equal12, equal23, equal13 ) =
+                        ( T.equal m1 m2
+                        , T.equal m2 m3
+                        , T.equal m1 m3
+                        )
+                in
+                (not equal12 || not equal23 || equal13)
+                    |> Expect.true "a & b => c   should be equiv to   -a | -b | c"
+        ]
